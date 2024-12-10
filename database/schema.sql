fb8091879457dd7d7f558ea1480a3fe6f9d3a57e
Ahmed Abdullah <ahmedabdullah2842004@gmail.com>
2024-12-10 07:54:38 -0800

    Added like a placeholder for the tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,57 @@
+-- Users Table
+CREATE TABLE Users (
+
+);
+
+-- UserPhones Table
+CREATE TABLE UserPhones (
+
+);
+
+-- Restaurants Table
+CREATE TABLE Restaurants (
+
+);
+
+-- RestaurantPhones Table
+CREATE TABLE UserPhones (
+
+);
+
+-- CusineType Table 
+CREATE TABLE CusineType (
+
+);
+
+-- Orders Table
+CREATE TABLE Orders (
+
+);
+
+-- ItemDetails Table
+CREATE TABLE ItemDetails (
+
+
+);
+
+-- MenuItems Table
+CREATE TABLE MenuItems (
+
+);
+
+
+-- Feedback Table
+CREATE TABLE Feedback (
+
+);
+
+-- Payments Table
+CREATE TABLE Payments (
+
+);
+ 
+-- Delivery Table
+CREATE TABLE Delivery (
+
+);
 
